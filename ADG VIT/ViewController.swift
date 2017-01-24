e805c5dca5a926a59db8777d55fd37888378a14f
Srivinayak Chaitanya Eshwa <eshwavin@hotmail.com>
2017-01-25 01:30:49 +0530

    Initial Commit

---
+++ ADG VIT/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ADG VIT
+//
+//  Created by Srivinayak Chaitanya Eshwa on 25/01/17.
+//  Copyright © 2017 Srivinayak Chaitanya Eshwa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
