98871ef248970aea968d27b7a340be0a643bd2e8
Srivinayak Chaitanya Eshwa <eshwavin@hotmail.com>
2017-03-04 23:22:44 +0530

    Final Changes Done

--- ADG VIT/AppDelegate.swift
+++ ADG VIT/AppDelegate.swift
@@ -154,8 +154,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterD
     func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
         completionHandler(
             [UNNotificationPresentationOptions.alert,
-             UNNotificationPresentationOptions.sound,
-             UNNotificationPresentationOptions.badge])
+             UNNotificationPresentationOptions.sound])
     }
     
     
